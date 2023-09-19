4f28bd986f3522b982005ac7ef2e2dd836a3dad8
gyeongisung <130673870+gyeongisung@users.noreply.github.com>
2023-09-19 17:54:55 +0900

    Update TeamFApplication.java

--- src/main/java/com/green/team_f/TeamFApplication.java
+++ src/main/java/com/green/team_f/TeamFApplication.java
@@ -2,6 +2,9 @@ package com.green.team_f;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class TeamFApplication {
@@ -9,5 +12,18 @@ public class TeamFApplication {
     public static void main(String[] args) {
         SpringApplication.run(TeamFApplication.class, args);
     }
+
+    @Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedHeaders("*") // 어떤 헤더들을 허용할 것인지
+						.allowedMethods("*") // 어떤 메서드를 허용할 것인지 (GET, POST...)
+						.allowedOrigins("https://web-travel-todo-cu6q2blkafu2i7.sel4.cloudtype.app");
+			}
+		};
+	}
 
 }
